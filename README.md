8881b6c95621697ab55b255d77fa75542d3cd8e8
Cristian Barbaro <cristiandanielbarbaro@gmail.com>
2020-04-09 15:01:33 -0300

    agregado playlist

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 ### Run youtube-music.sh. Urls in quotes:
 * `./youtube-music.sh "url_yt_1 url_yt_2 ..."`
 
-### Example:
-
-* `./youtube-music.sh "https://www.youtube.com/watch?v=D0yxAGyL3V4 https://www.youtube.com/watch?v=fjySbtWRGgI"`
+### Examples:
+* List of urls:
+    * `./youtube-music.sh -u "https://www.youtube.com/watch?v=D0yxAGyL3V4 https://www.youtube.com/watch?v=4-XayQ-IVig"`
+* Playlist:
+    * `./youtube-music.sh -p "https://www.youtube.com/watch?v=HXCyfOCRHOo&list=RDRscHbZIHaAU"`
--- youtube-music.sh
+++ youtube-music.sh
@@ -1,25 +1,60 @@
 #!/bin/bash
 
-URLS=$1
+play(){
+	VID=$1
+	if [[ $VID == *"mime=audio"* ]];
+	then
+		echo -e "Playing ${VID}...\n\nPress 'Ctrl + C' for next song.\n"
+		mplayer -cache 16384 -cache-min 80 $VID 2>/dev/null
+	fi
+}
 
-for URL in $URLS
-do
+play_urls() {
 
-	echo -e "Searching video url from YouTube for $URL...\n"
+	for URL in $URLS
+	do
+		echo -e "Searching video url from YouTube for $URL...\n"
+		
+		YT_VID_URLS=$(/usr/local/bin/youtube-dl -g "$URL")
 
-	YT_VID_URLS=$(/usr/local/bin/youtube-dl -g "$URL")
+		for VID in $YT_VID_URLS
+		do 
+			play $VID
+		done
+	done
 
-	YT_VID_URL=$(echo $YT_VID_URLS | cut -d" " -f2)
+	echo -e "No more links. Exiting..."
+}
 
-	echo -e "Playing ${URL}...\nPress 'Ctrl + C' for next song.\n"
 
-	mplayer -cache 16384 -cache-min 80 $YT_VID_URL 2>/dev/null
+play_list(){
 
-	echo -e "${URL} has played. Next...\n"
+	echo -e "Downloading playlist. Please, wait...\n"
 
-done
+	PLAYLIST=$(/usr/local/bin/youtube-dl -g --playlist-start 1 $URLS)
 
-echo -e "No more links. Exiting..."
+	for VID in $PLAYLIST
+	do 
+		play $VID
+	done
+
+	echo -e "No more playlist. Exiting..."
+}
+
+OPT=$1
+URLS=$2
+
+case $OPT in 
+
+	-u) play_urls;;
+
+	-p) play_list;;
+
+	*) echo "Option $1 not recognized.";;
+
+esac
+
+shift
 
 exit 0
  
